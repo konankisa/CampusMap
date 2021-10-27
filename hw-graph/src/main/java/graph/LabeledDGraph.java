569f6df0323fbd6b9526befcd3ee12303acb8a75
anish <skonanki@uw.edu>
2021-10-26 22:30:25 -0700

    Worked on the class and answers

--- hw-graph/src/main/java/graph/LabeledDGraph.java
+++ hw-graph/src/main/java/graph/LabeledDGraph.java
@@ -3,23 +3,24 @@ package graph;
 
 import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 
 /**
  * LabeledDGraph represents a mutable directed graph with a finite number of nodes
  * and edges associated with the nodes. Each node can only appear once in the graph.
- * An edge stores a label and points from 1 node to another. Edges can point to the same node
- * Edges cannot have null values, and neither can nodes
+ * A node is a string value stored in the graph.
+ * An edge stores a label and points from 1 node to another. Edges can point to the same node.
+ * Edges cannot have null values, and neither can nodes.
  *
  */
 public class LabeledDGraph {
 
     /**
-     * Constructor that creates a HashMap representing an empty graph
+     * Constructor that creates an empty graph
      *
-     * @spec.effects Constructs an empty HashMap
+     * @spec.effects Constructs an empty graph
      */
     public LabeledDGraph() {
+        throw new RuntimeException("Graph is not yet implemented");
     }
 
     /**
@@ -30,7 +31,7 @@ public class LabeledDGraph {
      * @spec.requires node != null
      * @return true if node was not already in graph and was added
      */
-    public boolean addNode (Node node) {
+    public boolean addNode (String node) {
         throw new RuntimeException("addNode is not yet implemented");
     }
 
@@ -44,7 +45,7 @@ public class LabeledDGraph {
      * @spec.requires head != null, tail != null, label != null
      * @return true if edge was added between 2 nodes
      */
-    public boolean addEdge (Node head, Node tail, Edge edge) {
+    public boolean addEdge (String head, String tail, String edge) {
         throw new RuntimeException("addEdge is not yet implemented");
     }
 
@@ -57,7 +58,7 @@ public class LabeledDGraph {
      * @return true if node was in the graph and was deleted
      * @throws IllegalArgumentException if node is not in graph
      */
-    public boolean removeNode (Node node) {
+    public boolean removeNode (String node) {
         throw new RuntimeException("removeNode is not yet implemented");
     }
 
@@ -71,7 +72,7 @@ public class LabeledDGraph {
      * @spec.requires head != null, tail != null, label != null
      * @return true if edge was removed between 2 nodes
      */
-    public boolean removeEdge (Node head, Node tail, Edge edge) {
+    public boolean removeEdge (String head, String tail, String edge) {
         throw new RuntimeException("removeEdge is not yet implemented");
     }
 
@@ -81,7 +82,7 @@ public class LabeledDGraph {
      * @param node the node to check for in the graph
      * @return true if the graph doesn't have the node
      */
-    public boolean containsNode(Node node) {
+    public boolean containsNode(String node) {
         throw new RuntimeException("containsNode is not yet implemented");
     }
 
@@ -108,76 +109,17 @@ public class LabeledDGraph {
      *
      * @return a list of all nodes in the graph
      */
-    public HashSet<Node> getNodes() {
+    public HashSet<String> getNodes() {
         throw new RuntimeException("getNodes is not yet implemented");
     }
 
     /**
-     * Returns a list of all edges in the graph
+     * Returns a list of all edge labels in the graph
      *
-     * @return a list of all edges in the graph
+     * @return a list of all edge labels in the graph
      */
-    public ArrayList<Edge> getEdges() {
+    public ArrayList<String> getEdges() {
         throw new RuntimeException("getEdges is not yet implemented");
     }
-
-    private class Edge {
-
-        /**
-         * Constructor that creates a labeled edge
-         *
-         * @param label the label of the edge
-         * @param child the node that the edge points to
-         * @spec.requires label != null, node != null
-         * @spec.effects Constructs an edge with a label, pointing to a node child
-         */
-        public Edge(String label, Node child) {
-            throw new RuntimeException("Edge is not yet implemented");
-        }
-
-        /**
-         * Returns the label of the edge
-         *
-         * @return The label of the edge
-         */
-        public String getLabel() {
-            throw new RuntimeException("getLabel is not yet implemented");
-        }
-
-        /**
-         * Returns the node that the edge is pointing to
-         *
-         * @return The child node that the edge points to
-         */
-        public Node getChild() {
-            throw new RuntimeException("getChild is not implemented");
-        }
-
-    }
-
-    private class Node {
-
-        /**
-         * Constructor that creates a node
-         *
-         * @param data the data of the node
-         * @spec.requires data != null
-         * @spec.effects Constructs a node with some data stored in it
-         */
-        public Node(String data) {
-            throw new RuntimeException("Node is not yet implemented");
-        }
-
-        /**
-         * Returns the data that is stored in the node
-         *
-         * @return The data stored in the node
-         */
-        public String getData() {
-            throw new RuntimeException("getData is not yet implemented");
-        }
-    }
-
 }
-
 
